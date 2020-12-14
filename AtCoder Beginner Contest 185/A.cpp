181afe892953b2ca07551683890f7a5d8a88ce0b
Hitesh Kumar Saini <hit7sh@gmail.com>
2020-12-14 14:11:10 +0530

    Create A.cpp

---
+++ AtCoder Beginner Contest 185/A.cpp
@@ -0,0 +1,18 @@
+// Author :: <Hitesh_Saini>
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){//G
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin); 
+		freopen("output.txt", "w", stdout);
+	#endif
+    __speed();
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << min({a, b, c, d});
+    return 0;
+}
